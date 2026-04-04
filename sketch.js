1658a998e3bbcca763ec468a3cd655521f0ecf0b
Fabian <fabrofabian@gmail.com>
2026-04-03 23:17:00 -0700

    reformated as class nodes for the boxes

--- sketch.js
+++ sketch.js
@@ -1,16 +1,77 @@
 let name = "Fihra";
 // let hydra;
 
-let allNotes = [];
+let allNodes = [];
 
 let nameNote;
 let isNameNoteOn = true;
 let nameNoteText;
 
+let nameNoteInlet;
+
 let xPos = 0;
 let speed = 0.6;
 
 let showNameBtn;
+
+class Node {
+    constructor(x, y, width, height, label, content) {
+        this.el = createDiv(label);
+        this.el.position(x, y);
+        this.el.size(width, height);
+        this.isNodeOn = true;
+
+        this.xPos = 0;
+        this.textSpeed = 0.6;
+
+        this.el.style('border', '2px solid #ccc');
+        this.el.style('box-shadow', '5px 5px 15px rgba(0,0,0,0.3)');
+        this.el.style('font-size', '20px');
+        this.el.style('background', 'green');
+        this.el.style('color', 'white');
+        this.el.style('padding', '5px');
+        this.el.style('overflow', 'hidden');
+
+        this.closeButton = createButton('X');
+        this.closeButton.parent(this.el);
+        this.closeButton.style('background-color', 'red');
+        this.closeButton.style('color', 'black');
+        this.closeButton.style('position', 'absolute');
+        this.closeButton.style('left', '85%');
+        this.closeButton.style('padding', '5px 5px');
+        this.closeButton.style('border-radius', '5px');
+
+        this.closeButton.mousePressed(() => this.onClose());
+
+
+        this.contentText = createP(content);
+        this.contentText.style('color', 'white');
+        this.contentText.style('position', 'absolute');
+        this.contentText.style('white-space', 'nowrap');
+        this.contentText.parent(this.el);
+
+        this.el.draggable();
+    }
+
+    onClose(){
+        console.log('hit close btn');
+        this.el.hide();
+        this.isNodeOn = false;
+    }
+
+    showInlet(){
+        rect(this.el.position().x, this.el.position().y + 100, 20, 20, 1);
+    }
+
+    showContent(){
+        this.xPos -= this.textSpeed;
+        let textWidth = this.contentText.width;
+        if(this.xPos < -textWidth){
+            this.xPos = this.el.width;
+        }
+        this.contentText.style('left', `${this.xPos}px`);
+    }
+}
 
 function showNameNote(){
     nameNote = createDiv(name);
@@ -57,18 +118,21 @@ function showNameNote(){
 function setup() {
     let mainCanvas = createCanvas(windowWidth, windowHeight);
 
-    showNameBtn = createButton('Show Name');
-    showNameBtn.size(100, 40);
-    showNameBtn.position(10, 10);
+    // showNameBtn = createButton('Show Name');
+    // showNameBtn.size(100, 40);
+    // showNameBtn.position(10, 10);
     
-    showNameBtn.mousePressed(() => {
-        console.log('hit test button');
-        console.log(isNameNoteOn);
-        isNameNoteOn = true;
-    });
+    // showNameBtn.mousePressed(() => {
+    //     console.log('hit test button');
+    //     console.log(isNameNoteOn);
+    //     isNameNoteOn = true;
+    // });
     
-    showNameNote();
+    // showNameNote();
 
+    let nameNode = new Node(500, 20, 300, 100, "Fihra", "Creative Technologist | Game Audio | Rondalla & Kulintang Musician | Composer");
+
+    allNodes.push(nameNode);
 
 //   const hCanvas = document.getElementById("hydra-canvas");
 //   hCanvas.width = windowWidth;
@@ -81,6 +145,11 @@ function setup() {
 // //   hydra.s0.init({ src: p5Canvas.elt});
 }
 
+function inlet(element){
+    console.log("element: ", element);
+    rect(element.x, element.y, 100, 20, 20, 1);
+}
+
 function draw() {
   background(220);
   text(name, width/2, height/2);
@@ -89,19 +158,27 @@ function draw() {
   background(0);
   fill(255);
 
-    if(isNameNoteOn){
-        nameNote.show();
-    } 
-
-    xPos -= speed;
-    let textWidth = nameNoteText.width;
-    if(xPos < -textWidth){
-        xPos = nameNote.width;
+    for(let i = 0; i < allNodes.length; i++){
+        if(allNodes[i].isNodeOn){
+            allNodes[i].el.show();
+            allNodes[i].showInlet();
+            allNodes[i].showContent();
+        }
     }
-    nameNoteText.style('left', `${xPos}px`);
+
+    // if(isNameNoteOn){
+    //     nameNote.show();
+    //     rect(nameNote.position().x, nameNote.position().y + 100, 20, 20, 1);
+    // } 
+
+    // xPos -= speed;
+    // let textWidth = nameNoteText.width;
+    // if(xPos < -textWidth){
+    //     xPos = nameNote.width;
+    // }
+    // nameNoteText.style('left', `${xPos}px`);
 
 //   ellipse(mouseX, mouseY, 50, 50);
-
 //   hydra.osc().out();
 
 
